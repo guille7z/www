2bc72d271e742406b55f7e1a42211f51d69e3930
guille7z <271281557+guille7z@users.noreply.github.com>
2026-08-01 19:35:45 +0200

    empty everything - to be redone

--- src/routes/blog/[slug]/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types.ts';
-
-export const load: PageLoad = async ({ params }) => {
-    const post = await import(`../../../posts/${params.slug}.md`);
-    return {
-        content: post.default,
-        meta: post.metadata as Record<string, string>
-    };
-};
